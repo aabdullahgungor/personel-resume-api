b1387ca502180fc6bbbdb6794a9364603bac97ae
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-04 01:22:06 +0300

    personal model file added

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/aabdullahgungor/personal-resume-api
 
 go 1.19
+
+require gorm.io/gorm v1.24.6
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
---
+++ internal/model/personal_model.go
@@ -0,0 +1,23 @@
+package model
+
+import "gorm.io/gorm"
+
+type Personal struct {
+	gorm.Model
+	Name     string `json:"name" gorm:"not null" `
+	Surname  string `json:"surname" gorm:"not null"`
+	UserName string `json:"username" gorm:"not null;unique"`
+	Email    string `json:"email" gorm:"not null;unique"`
+	Password string `json:"password" gorm:"not null"`
+}
+
+type Authentication struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Role        string `json:"role"`
+	Email       string `json:"email"`
+	TokenString string `json:"token"`
+}
