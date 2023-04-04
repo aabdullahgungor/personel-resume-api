c6a5d9ba5537d51b6cc883b9fe5924b3feb05a2f
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-04 19:42:50 +0300

    university repository file added

---
+++ internal/repository/university_repository.go
@@ -0,0 +1,86 @@
+package repository
+
+import (
+	"errors"
+	"log"
+	"strconv"
+
+	"github.com/aabdullahgungor/personal-resume-api/internal/database"
+	"github.com/aabdullahgungor/personal-resume-api/internal/model"
+	"gorm.io/gorm"
+)
+
+var (
+	ErrUniversityNotFound = errors.New("FromRepository - university not found")
+)
+
+type PostgreSqlUniversityRepository struct {
+	connectionPool *gorm.DB
+}
+
+func NewPostgreSqlUniversityRepository() *PostgreSqlUniversityRepository {
+
+	db := database.GetDatabase()
+
+	return &PostgreSqlUniversityRepository{
+		connectionPool: db,
+	}
+}
+
+
+func (p *PostgreSqlUniversityRepository) GetUniversities() ([]model.University, error){
+
+	var universities []model.University
+	err := p.connectionPool.Find(&universities)
+	if err != nil {
+        return []model.University{}, ErrUniversityNotFound
+	}
+
+	return universities, nil
+}
+func (p *PostgreSqlUniversityRepository) GetUniversityById(id int) (model.University, error) {
+
+	var university model.University
+	err := p.connectionPool.First(&university, id)
+	if err != nil {
+		return model.University{}, ErrUniversityNotFound
+	}
+
+	return university, nil
+}
+func (p *PostgreSqlUniversityRepository) CreateUniversity(university *model.University) error {
+
+	err := p.connectionPool.Create(&university).Error
+
+	if err != nil {
+        panic(err)
+	}
+
+	log.Printf("\ndisplay the ids of the newly inserted objects: %v", university.ID)
+
+	return err
+}
+func (p *PostgreSqlUniversityRepository) EditUniversity(university *model.University) error {
+
+	err := p.connectionPool.Save(&university).Error
+
+	if err != nil {
+        panic(err)
+	}
+
+	log.Printf("\ndisplay the ids of the edited objects: %v", university.ID)
+
+	return err
+}
+func (p *PostgreSqlUniversityRepository) DeleteUniversity(id int) error {
+
+	err := p.connectionPool.Delete(&model.University{}, id).Error
+
+	if err != nil {
+        panic(err)
+	}
+
+	log.Println("deleting the first result from the search filter\n"+ "The id of the deleted document:"+strconv.Itoa(id))
+
+	return err
+}
