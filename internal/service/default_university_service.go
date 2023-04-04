1e59d490a3fc02b2865a8a1e264cbc8c96e13ea7
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-05 01:11:49 +0300

    university service file added

---
+++ internal/service/default_university_service.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/aabdullahgungor/personal-resume-api/internal/model"
+	"github.com/aabdullahgungor/personal-resume-api/internal/repository"
+)
+
+var ( 
+	ErrUniversityIDIsNotValid       = errors.New("University id is not valid")
+	ErrUniversityNotFound           = errors.New("University cannot be found")
+)
+
+
+type DefaultUniversityService struct {
+	universityRepo repository.IUniversityRepository
+}
+
+func NewDefaultUniversityService(uRepo repository.IUniversityRepository) *DefaultUniversityService {
+	return &DefaultUniversityService{
+		universityRepo: uRepo,
+	}
+}
+
+func (d *DefaultUniversityService) GetAll() ([]model.University, error) {
+	return d.universityRepo.GetAllUniversities()
+}
+func (d *DefaultUniversityService) GetById(id string) (model.University, error) {
+
+	int_id, errId := strconv.Atoi(id)
+	if errId != nil {
+		return model.University{}, errId
+	}
+	university, err := d.universityRepo.GetUniversityById(int_id)
+
+	if err != nil {
+		return model.University{}, ErrUniversityNotFound
+	}
+
+	return university, nil
+}
+func (d *DefaultUniversityService) Create(university *model.University) error {
+	return d.universityRepo.CreateUniversity(university)
+}
+func (d *DefaultUniversityService) Edit(university *model.University) error {
+	err := d.universityRepo.EditUniversity(university)
+	if err != nil {
+		return ErrUniversityNotFound
+	}
+
+	return nil
+}
+func (d *DefaultUniversityService) Delete(id string) error {
+	int_id, errId := strconv.Atoi(id)
+	if errId != nil {
+		return errId
+	}
+	err := d.universityRepo.DeleteUniversity(int_id)
+
+	if err != nil {
+		return ErrUniversityNotFound
+	}
+
+	return nil
+}
+
