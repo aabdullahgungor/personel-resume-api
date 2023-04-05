2b820d94e843daf9b963308355f40ffdad016b1d
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-04-06 02:39:10 +0300

    cmd file updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/aabdullahgungor/personal-resume-api/internal/database"
+	"github.com/aabdullahgungor/personal-resume-api/internal/server"
+)
+
 func main() {
+	database.StartDB()
+	s := server.NewServer()
+	s.Run()
 
 }
