4f7211ff9e850175553e8632cb0e4a06150b8ca4
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-05-23 17:15:54 +0300

    token controller file created

---
+++ internal/controller/token_controller.go
@@ -0,0 +1,51 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/aabdullahgungor/personal-resume-api/internal/auth"
+	"github.com/aabdullahgungor/personal-resume-api/internal/model"
+	"github.com/aabdullahgungor/personal-resume-api/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+type tokenController struct {
+	service service.IPersonalService
+}
+
+func NewTokenController(ts service.IPersonalService) *tokenController {
+	return &tokenController{
+		service: ts,
+	}
+}
+
+func (ts *tokenController) GenerateToken(context *gin.Context) {
+	var request model.Authentication
+	//var personal model.Personal
+	err := context.ShouldBindJSON(&request)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	// check if email exists and password is correct
+	personal, err := ts.service.GetByEmail(request.Email)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		context.Abort()
+		return
+	}
+	credentialError := personal.CheckPassword(request.Password)
+	if credentialError != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+		context.Abort()
+		return
+	}
+	tokenString, err := auth.GenerateJWT(personal.Email, personal.UserName)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		context.Abort()
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
