00a9d8a6ca20454ff990a5bdaf1dbcccd7eaa070
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-05-23 17:22:36 +0300

    middleware auth file created for JWT

---
+++ internal/middleware/auth.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"github.com/aabdullahgungor/personal-resume-api/internal/auth"
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		tokenString := context.GetHeader("Authorization")
+		if tokenString == "" {
+			context.JSON(401, gin.H{"error": "request does not contain an access token"})
+			context.Abort()
+			return
+		}
+		err := auth.ValidateToken(tokenString)
+		if err != nil {
+			context.JSON(401, gin.H{"error": err.Error()})
+			context.Abort()
+			return
+		}
+		context.Next()
+	}
+}
