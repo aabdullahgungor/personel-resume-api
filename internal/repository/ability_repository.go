31478e3543deb4646e320a826533a745bdd22740
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-06-09 22:59:04 +0300

    ability repository file updated

--- internal/repository/ability_repository.go
+++ internal/repository/ability_repository.go
@@ -10,14 +10,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type IAbilityRepository interface {
-	GetAllAbilities() ([]model.Ability, error)
-	GetAbilityById(id int) (model.Ability, error)
-	CreateAbility(ability *model.Ability) error
-	EditAbility(ability *model.Ability) error
-	DeleteAbility(id int) error
-}
-
 var (
 	ErrAbilityNotFound = errors.New("FromRepository - ability not found")
 )
@@ -35,13 +27,12 @@ func NewPostgreSqlAbilityRepository() *PostgreSqlAbilityRepository {
 	}
 }
 
-
-func (p *PostgreSqlAbilityRepository) GetAllAbilities() ([]model.Ability, error){
+func (p *PostgreSqlAbilityRepository) GetAllAbilities() ([]model.Ability, error) {
 
 	var abilities []model.Ability
 	result := p.connectionPool.Find(&abilities)
 	if result.Error != nil {
-        return []model.Ability{}, ErrAbilityNotFound
+		return []model.Ability{}, ErrAbilityNotFound
 	}
 
 	return abilities, nil
@@ -61,7 +52,7 @@ func (p *PostgreSqlAbilityRepository) CreateAbility(ability *model.Ability) erro
 	err := p.connectionPool.Create(&ability).Error
 
 	if err != nil {
-        panic(err)
+		panic(err)
 	}
 
 	log.Printf("\ndisplay the ids of the newly inserted objects: %v", ability.ID)
@@ -73,7 +64,7 @@ func (p *PostgreSqlAbilityRepository) EditAbility(ability *model.Ability) error 
 	err := p.connectionPool.Save(&ability).Error
 
 	if err != nil {
-        panic(err)
+		panic(err)
 	}
 
 	log.Printf("\ndisplay the ids of the edited objects: %v", ability.ID)
@@ -85,10 +76,10 @@ func (p *PostgreSqlAbilityRepository) DeleteAbility(id int) error {
 	err := p.connectionPool.Delete(&model.Ability{}, id).Error
 
 	if err != nil {
-        panic(err)
+		panic(err)
 	}
 
-	log.Println("deleting the first result from the search filter\n"+ "The id of the deleted document:"+strconv.Itoa(id))
+	log.Println("deleting the first result from the search filter\n" + "The id of the deleted document:" + strconv.Itoa(id))
 
 	return err
 }
