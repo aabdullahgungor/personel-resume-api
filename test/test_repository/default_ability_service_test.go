34950b5a97aa527d4e36c23b1eb6c02dadb8c0d7
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-06-10 14:04:17 +0300

    ability service test file updated

--- test/test_repository/default_ability_service_test.go
+++ test/test_repository/default_ability_service_test.go
@@ -1,1 +1,106 @@
 package test_repository
+
+import (
+	"testing"
+
+	"github.com/aabdullahgungor/personal-resume-api/internal/model"
+	"github.com/aabdullahgungor/personal-resume-api/internal/repository"
+	"github.com/aabdullahgungor/personal-resume-api/internal/repository/mocks"
+	"github.com/aabdullahgungor/personal-resume-api/internal/service"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultAbilityService_GetAll(t *testing.T) {
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIAbilityRepository(mockCtrl)
+	mockRepository.EXPECT().GetAllAbilities().Return([]model.Ability{{
+		AbilityName: "Go",
+	},
+		{
+			AbilityName: "Ruby",
+		}}, nil)
+
+	abilityService := service.NewDefaultAbilityService(mockRepository)
+	abilities, err := abilityService.GetAll()
+
+	if assert.Nil(t, err) {
+		if len(abilities) == 2 {
+			t.Log("Ability counts is matching, func run succesfuly")
+		} else {
+			t.Log("Ability counts not matching, there is a problem in func")
+		}
+	} else {
+		t.Log(err)
+	}
+}
+
+func TestDefaultAbilityService_GetById(t *testing.T) {
+	id := "1"
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIAbilityRepository(mockCtrl)
+	mockRepository.EXPECT().GetAbilityById(gomock.Eq(id)).Return(model.Ability{}, repository.ErrAbilityNotFound)
+
+	abilityService := service.NewDefaultAbilityService(mockRepository)
+	_, err := abilityService.GetById(id)
+
+	assert.ErrorIs(t, err, repository.ErrAbilityNotFound)
+}
+
+func TestDefaultProductService_Create(t *testing.T) {
+
+	ability := model.Ability{AbilityName: "Go"}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIAbilityRepository(mockCtrl)
+	mockRepository.EXPECT().CreateAbility(&ability).Return(nil).Times(1)
+
+	abilityService := service.NewDefaultAbilityService(mockRepository)
+	err := abilityService.Create(&ability)
+
+	if assert.Nil(t, err) {
+		t.Log("Success Create Product")
+	} else {
+		t.Log("Product cannot create")
+	}
+
+}
+
+func TestDefaultProductService_Edit(t *testing.T) {
+
+	ability := model.Ability{AbilityName: "Go"}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIAbilityRepository(mockCtrl)
+	mockRepository.EXPECT().EditAbility(&ability).Return(nil).Times(1)
+
+	abilityService := service.NewDefaultAbilityService(mockRepository)
+	err := abilityService.Edit(&ability)
+
+	if assert.Nil(t, err) {
+		t.Log("Success Update Ability")
+	} else {
+		t.Log("Ability cannot update")
+	}
+}
+
+func TestDefaultProductService_Delete(t *testing.T) {
+
+	id := "1"
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIAbilityRepository(mockCtrl)
+	mockRepository.EXPECT().DeleteAbility(gomock.Eq(id)).Return(nil).Times(1)
+
+	abilityService := service.NewDefaultAbilityService(mockRepository)
+	err := abilityService.Delete(id)
+
+	if assert.Nil(t, err) {
+		t.Log("Success delete Ability")
+	} else {
+		t.Log("Ability cannot delete")
+	}
+}
