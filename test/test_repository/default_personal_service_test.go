361522bce626d4d22a78ad7ce2a3482e4341d312
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-06-10 15:43:51 +0300

    personal service test file updated

--- test/test_repository/default_personal_service_test.go
+++ test/test_repository/default_personal_service_test.go
@@ -1,1 +1,130 @@
 package test_repository
+
+import (
+	"testing"
+
+	"github.com/aabdullahgungor/personal-resume-api/internal/model"
+	"github.com/aabdullahgungor/personal-resume-api/internal/repository"
+	"github.com/aabdullahgungor/personal-resume-api/internal/repository/mocks"
+	"github.com/aabdullahgungor/personal-resume-api/internal/service"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultPersonalService_GetAll(t *testing.T) {
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIPersonalRepository(mockCtrl)
+	mockRepository.EXPECT().GetAllPersonals().Return([]model.Personal{{
+		Name:     "Abdullah",
+		Surname:  "Gungor",
+		UserName: "abdullahgungor",
+		Email:    "abdullahgungor@hotmail.com.tr",
+		Password: "123456",
+		UserType: "admin",
+	},
+		{
+			Name:     "Ismail",
+			Surname:  "Gungor",
+			UserName: "ismailgungor",
+			Email:    "ismailgungor@hotmail.com",
+			Password: "123456",
+			UserType: "user",
+		}}, nil)
+
+	personalService := service.NewDefaultPersonalService(mockRepository)
+	personals, err := personalService.GetAll()
+
+	if assert.Nil(t, err) {
+		if len(personals) == 2 {
+			t.Log("Personal counts is matching, func run succesfuly")
+		} else {
+			t.Log("Personal counts not matching, there is a problem in func")
+		}
+	} else {
+		t.Log(err)
+	}
+}
+
+func TestDefaultPersonalService_GetById(t *testing.T) {
+	id := "1"
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIPersonalRepository(mockCtrl)
+	mockRepository.EXPECT().GetPersonalById(gomock.Eq(id)).Return(model.Personal{}, repository.ErrPersonalNotFound)
+
+	personalService := service.NewDefaultPersonalService(mockRepository)
+	_, err := personalService.GetById(id)
+
+	assert.ErrorIs(t, err, repository.ErrPersonalNotFound)
+}
+
+func TestDefaultPersonalService_Create(t *testing.T) {
+
+	personal := model.Personal{
+		Name:     "Abdullah",
+		Surname:  "Gungor",
+		UserName: "abdullahgungor",
+		Email:    "abdullahgungor@hotmail.com.tr",
+		Password: "123456",
+		UserType: "admin",
+	}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIPersonalRepository(mockCtrl)
+	mockRepository.EXPECT().CreatePersonal(&personal).Return(nil).Times(1)
+
+	personalService := service.NewDefaultPersonalService(mockRepository)
+	err := personalService.Create(&personal)
+
+	if assert.Nil(t, err) {
+		t.Log("Success Create Personal")
+	} else {
+		t.Log("Personal cannot create")
+	}
+
+}
+
+func TestDefaultPersonalService_Edit(t *testing.T) {
+
+	personal := model.Personal{
+		Name:     "Abdullah",
+		Surname:  "Gungor",
+		UserName: "abdullahgungor",
+		Email:    "abdullahgungor@hotmail.com.tr",
+		Password: "123456",
+		UserType: "admin",
+	}
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIPersonalRepository(mockCtrl)
+	mockRepository.EXPECT().EditPersonal(&personal).Return(nil).Times(1)
+
+	personalService := service.NewDefaultPersonalService(mockRepository)
+	err := personalService.Edit(&personal)
+
+	if assert.Nil(t, err) {
+		t.Log("Success Update Personal")
+	} else {
+		t.Log("Personal cannot update")
+	}
+}
+
+func TestDefaultPersonalService_Delete(t *testing.T) {
+
+	id := "1"
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockRepository := mocks.NewMockIPersonalRepository(mockCtrl)
+	mockRepository.EXPECT().DeletePersonal(gomock.Eq(id)).Return(nil).Times(1)
+
+	personalService := service.NewDefaultPersonalService(mockRepository)
+	err := personalService.Delete(id)
+
+	if assert.Nil(t, err) {
+		t.Log("Success delete Personal")
+	} else {
+		t.Log("Personal cannot delete")
+	}
+}
