e80a25645a3bc5b6d0e5948ac0a1b43ca6a05dc5
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-06-11 17:33:13 +0300

    experience controller file updated for swagger

--- internal/controller/experience_controller.go
+++ internal/controller/experience_controller.go
@@ -20,6 +20,13 @@ func NewExperienceController(es interfaces.IExperienceService) *experienceContro
 	}
 }
 
+// GetExperiences           godoc
+// @Summary		Get experiences   array
+// @Description	Responds with the list of all experiences   as JSON.
+// @Tags			experiences
+// @Produce		json
+// @Success		200	{object}	model.Experience
+// @Router			/experiences   [get]
 func (es *experienceController) GetAllExperiences(context *gin.Context) {
 	experiences, err := es.service.GetAll()
 	if err != nil {
@@ -29,6 +36,17 @@ func (es *experienceController) GetAllExperiences(context *gin.Context) {
 	context.Header("Content-Type", "application/json")
 	context.IndentedJSON(http.StatusOK, experiences)
 }
+
+// GetExperience           godoc
+// @Summary		Get single experience by id
+// @Description	Returns the experience whose id value matches the id.
+// @Tags			experiences
+// @Produce		json
+// @Param			id path	string true "search experience by id"
+// @Success		200		{object}	model.Experience
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/experiences/{id} [get]
 func (es *experienceController) GetExperienceById(context *gin.Context) {
 	str_id := context.Param("id")
 	experience, err := es.service.GetById(str_id)
@@ -46,6 +64,17 @@ func (es *experienceController) GetExperienceById(context *gin.Context) {
 	context.Header("Content-Type", "application/json")
 	context.IndentedJSON(http.StatusOK, experience)
 }
+
+// CreateExperience           godoc
+// @Summary		Add a new experience
+// @Description	Takes a experience  JSON and store in DB. Return saved JSON.
+// @Tags			experiences
+// @Produce		json
+// @Param			experience  body	model.Experience  true "Experience  JSON"
+// @Success		200		{object}	model.Experience
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/experiences [post]
 func (es *experienceController) CreateExperience(context *gin.Context) {
 	var experience model.Experience
 	err := context.ShouldBindJSON(&experience)
@@ -67,6 +96,17 @@ func (es *experienceController) CreateExperience(context *gin.Context) {
 	}
 	context.IndentedJSON(http.StatusCreated, gin.H{"message": "Experience has been created"})
 }
+
+// EditExperience          godoc
+// @Summary		Edit an experience
+// @Description	Takes a experience  JSON and edit an in DB. Return saved JSON.
+// @Tags			experiences
+// @Produce		json
+// @Param			experience  body	model.Experience 	true "Experience  JSON"
+// @Success		200		{object}	model.Experience
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/experiences  [put]
 func (es *experienceController) EditExperience(context *gin.Context) {
 
 	var experience model.Experience
@@ -90,6 +130,17 @@ func (es *experienceController) EditExperience(context *gin.Context) {
 
 	context.IndentedJSON(http.StatusCreated, gin.H{"message": "Experience has been edited", "experience_id": experience.ID})
 }
+
+// DeleteExperience           godoc
+// @Summary		Delete an experience
+// @Description	Remove an experience  from DB by id.
+// @Tags			experiences
+// @Produce		json
+// @Param			id path	string true "delete experience  by id"
+// @Success		200		{object}	model.Experience
+// @Failure 	400     error message
+// @Failure 	406     error message
+// @Router			/experiences/{id} [delete]
 func (es *experienceController) DeleteExperience(context *gin.Context) {
 	str_id := context.Param("id")
 	err := es.service.Delete(str_id)
