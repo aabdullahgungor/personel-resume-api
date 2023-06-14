f71e82fc1ca6eac070935e2180e882a62c020638
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-06-14 04:00:31 +0300

    server file updated

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,22 +8,22 @@ import (
 )
 
 type Server struct {
-	port string
+	port   string
 	server *gin.Engine
 }
 
 func NewServer() Server {
 	return Server{
-		port: "8080",
+		port:   "8080",
 		server: gin.Default(),
 	}
 }
 
 func (s *Server) Run() {
-	gin.SetMode(gin.ReleaseMode)
+	//gin.SetMode(gin.ReleaseMode)
 	router := router.ConfigRouters(s.server)
-	
+
 	log.Printf("Server running at port: %v", s.port)
 	log.Fatal(router.Run("localhost:" + s.port))
-	
+
 }
